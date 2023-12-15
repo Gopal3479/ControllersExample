1b4cfeff61475c8c0685db7267f2ff1ad5a25319
Gopal reddy <72065198+gopal5288@users.noreply.github.com>
2023-12-16 01:49:58 +0530

    Add project files.

---
+++ ControllersExample/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllersExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("Home")]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();// if we dont specfy the view by defaultly it will take Index.cshtml
+            //Views/Home/Index.cshtml
+        }
+    }
+}
---
+++ ControllersExample/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace ControllersExample.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+        public Gender Gender { get; set; }
+        
+    }
+
+    public enum Gender
+    {
+        Male,Female,Other
+    };
+}
